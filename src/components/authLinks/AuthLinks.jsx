40a6d772f7f8f7e65693e916c600fa37031243fa
ahihi <bdsthocu20@gmail.com>
2023-11-20 17:51:12 +0700

    #1

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,10 @@
+import styles from "./authLinks.module.css"
+
+const AuthLinks = () => {
+    return (
+        <div className={styles.container}>AuthLinks</div>
+
+    );
+};
+
+export default AuthLinks;
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+import styles from "./themeToggle.module.css"
+
+const ThemeToggle = () => {
+    return (
+        <div className={styles.container}>ThemeToggle</div>
+
+    );
+};
+
+export default ThemeToggle;
