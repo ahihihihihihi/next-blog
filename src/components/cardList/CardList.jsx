4411ff6d0e1e24ae1e35dc897c90b1b8a7af6241
ahihi <bdsthocu20@gmail.com>
2023-11-23 11:56:14 +0700

    #9

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -2,7 +2,20 @@ import Card from "../card/Card";
 import Pagination from "../pagination/Pagination";
 import styles from "./cardList.module.css"
 
-const CardList = () => {
+const getData = async () => {
+    const res = await fetch("http://localhost:3000/api/posts", {
+        cache: "no-store",
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed");
+    }
+
+    return res.json();
+};
+
+const CardList = async () => {
+    const data = await getData();
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Recent Posts</h1>
