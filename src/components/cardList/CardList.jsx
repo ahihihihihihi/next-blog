6c7b06768838be2919e9729fae5ea5dbff13f064
ahihi <bdsthocu20@gmail.com>
2023-11-23 15:29:58 +0700

    #11

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -16,15 +16,19 @@ const getData = async (page) => {
 };
 
 const CardList = async ({ page }) => {
-    const data = await getData(page);
+    const { posts, count } = await getData(page);
     // console.log(">>>check data: ", data)
+
+    const POST_PER_PAGE = 2;
+
+    const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+    const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Recent Posts</h1>
             <div className={styles.posts}>
                 {
-                    data && data.length &&
-                    data?.map((item) => {
+                    posts?.map((item) => {
                         return (
                             <>
                                 <Card
@@ -38,6 +42,8 @@ const CardList = async ({ page }) => {
             </div>
             <Pagination
                 page={page}
+                hasPrev={hasPrev}
+                hasNext={hasNext}
             />
         </div>
     );
